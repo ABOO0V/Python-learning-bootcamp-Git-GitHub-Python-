0d7c24d66e7eb6fcc21559bad5b4a68f662a9c5a
GitGitHubPython <a.b.arafatbhuiyan.9@gmail.com>
2026-05-03 01:10:20 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Python-learning-bootcamp-Git-GitHub-Python-
---
+++ print.py
@@ -0,0 +1,40 @@
+print()
+
+print("This is a string in python")
+print("Hello, World")
+print("Hello, GitHub!")
+print('21+1')
+
+print()
+
+print("Name: Bob \nAge:19")
+print("Name: Bob2 \n\tAge2:20")
+
+# \n = new line
+# \t = Tab or 4 space
+
+print()
+
+print(f"Name: John, Age:{21+1}")
+print(f"Name: John \nAge:{20}\nAddress: W.D. USA")
+
+print()
+
+print("""Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.
+Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+Excepteur sint occaecat cupidatat non proident,
+sunt in culpa qui officia deserunt mollit anim id est laborum.
+ """)
+
+print()
+
+''' multiline comment'''
+# Single line comment
+""" Also 
+    this one
+"""
